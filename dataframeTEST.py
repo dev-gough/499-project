4a6b4f1468ba08288c394d47461c68ff7cb99174
Simsyb <99280088+Simsyb@users.noreply.github.com>
2022-03-09 14:37:50 -0500

    Update dataframeTEST.py

--- dataframeTEST.py
+++ dataframeTEST.py
@@ -1,23 +1,20 @@
-"""
-CISC 499 Project
-"""
-
 import glob
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def get_dataframes(bound1,bound2):
+def get_dataframes_inter(bound1,bound2):
     international_path = 'data/International/'
     international_files = [international_path + str(i) + ".csv".format(i) for i in range(bound1, bound2+1)]
     international_df = pd.concat((pd.read_csv(f) for f in international_files), ignore_index=True)
-    international_df.to_pickle('international.pkl')
+    return(international_df)
 
+def get_dataframes_dom(bound1, bound2):
     domestic_path = 'data/Domestic/'
     domestic_files = [domestic_path + str(i) + ".csv".format(i) for i in range(bound1, bound2 + 1)]
     domestic_df = pd.concat((pd.read_csv(f) for f in domestic_files), ignore_index=True)
-    domestic_df.to_pickle('domestic.pkl')
+    return(domestic_df)
 
 
 
